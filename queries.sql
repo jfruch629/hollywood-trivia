d4f4a395e3c248841d1200ad6e6e5b8de42e4a8f
Jake Fruci <jfruch629@gmail.com>
2017-11-30 18:44:03 -0500

    finished

---
+++ queries.sql
@@ -0,0 +1,76 @@
+-- 1. What are the top 50 worst rated movies? The results should
+-- include the movie title and rating and be sorted by the worst
+-- rating first.
+
+SELECT movies.title, movies.rating
+FROM movies
+ORDER BY movies.rating ASC
+LIMIT 50;
+
+
+-- 2. What movies do not have a rating? The results should include
+-- just the movie titles in sorted order.
+
+SELECT movies.title
+  FROM movies
+  WHERE movies.rating IS NULL
+  ORDER by movies.title DESC;
+
+
+-- 3. What movies have the word "thrilling" in their synopsis? The
+-- results should just include the movie title.
+
+SELECT movies.title
+  FROM movies
+  WHERE movies.synopsis LIKE '%thrilling%';
+
+
+-- 4. What were the highest rated 'Science Fiction & Fantasy' movies
+-- released in the 80's? The results should include the movie title,
+-- the year released, and rating sorted by highest rating first.
+
+SELECT movies.title, movies.year, movies.rating
+  FROM movies
+  WHERE movies.genre_id = 17
+  AND movies.year > 1979
+  AND movies.year < 1990
+  AND movies.rating > 75
+  ORDER BY movies.rating DESC;
+
+
+-- 5. What actors have starred as James Bond? The results should
+-- include the actor name, movie title, year released, and be sorted
+-- by year in ascending order (earliest year appears first).
+
+SELECT actors.name, movies.title, movies.year
+  FROM movies
+  JOIN cast_members ON cast_members.movie_id = movies.id
+  JOIN actors ON actors.id = cast_members.actor_id
+  WHERE cast_members.character = 'James Bond'
+  ORDER BY movies.year ASC;
+
+
+-- 6. What movies has Julianne Moore starred in? The results should
+-- include the movie title, year released, and name of the genre,
+-- sorted by genre first and then movie title.
+
+SELECT movies.title, movies.year, genres.name AS genre
+  FROM movies
+  JOIN cast_members ON movies.id = cast_members.movie_id
+  JOIN genres ON genres.id = movies.genre_id
+  JOIN actors ON actors.id = cast_members.actor_id
+  WHERE actors.name = 'Julianne Moore'
+  ORDER BY genres.name, movies.title ASC;
+
+
+-- 7. What were the five earliest horror movies and what studios
+-- produced them? Include the movie title, year released, and studio
+-- name (if any) in the results sorted by year.
+
+SELECT movies.title, movies.year, studios.name
+  FROM movies
+  JOIN studios ON movies.studio_id = studios.id
+  JOIN genres ON movies.genre_id = genres.id
+  WHERE movies.genre_id = 2
+  ORDER BY movies.year ASC
+  LIMIT 5;
